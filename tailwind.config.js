cfed92decdb83647510596540c5d9bba04af985b
achdiop <137773804+achdiop@users.noreply.github.com>
2026-07-26 23:02:01 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,62 @@
+const colors = require('tailwindcss/colors');
+
+const gray = {
+    50: 'hsl(216, 33%, 97%)',
+    100: 'hsl(214, 15%, 91%)',
+    200: 'hsl(210, 16%, 82%)',
+    300: 'hsl(211, 13%, 65%)',
+    400: 'hsl(211, 10%, 53%)',
+    500: 'hsl(211, 12%, 43%)',
+    600: 'hsl(209, 14%, 37%)',
+    700: 'hsl(209, 18%, 30%)',
+    800: 'hsl(209, 20%, 25%)',
+    900: 'hsl(210, 24%, 16%)',
+};
+
+module.exports = {
+    content: [
+        './resources/scripts/**/*.{js,ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                header: ['"IBM Plex Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
+            },
+            colors: {
+                black: '#131a20',
+                // "primary" and "neutral" are deprecated, prefer the use of "blue" and "gray"
+                // in new code.
+                primary: {
+                    50: '#faf5ff',
+                    100: '#f3e8ff',
+                    200: '#e9d5ff',
+                    300: '#d8b4fe',
+                    400: '#c084fc',
+                    500: '#a855f7',
+                    600: '#9333ea',
+                    700: '#7e22ce',
+                    800: '#6b21a8',
+                    900: '#581c87',
+                },
+                gray: gray,
+                neutral: gray,
+                cyan: colors.cyan,
+            },
+            fontSize: {
+                '2xs': '0.625rem',
+            },
+            transitionDuration: {
+                250: '250ms',
+            },
+            borderColor: theme => ({
+                default: theme('colors.neutral.400', 'currentColor'),
+            }),
+        },
+    },
+    plugins: [
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/forms')({
+            strategy: 'class',
+        }),
+    ]
+};
